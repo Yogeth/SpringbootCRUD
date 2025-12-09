64e07af26108264b0a1d376ead14ac183a89ee3e
Yogeth <yogethcr@gmail.com>
2025-12-09 11:44:06 +0530

    update Studentservice.java

--- demo/src/main/java/com/example/demo/service/Studentservice.java
+++ demo/src/main/java/com/example/demo/service/Studentservice.java
@@ -3,11 +3,11 @@ import org.springframework.stereotype.Service;
 import java.util.*;
 import com.example.demo.entity.Student;
 @Service
-class Studentservice{
+public class Studentservice{
   List<Student> students=new ArrayList<>(
     Arrays.asList(
-      new Student(1,"Yoge","Blockchain");
-      new Student(2,"Yogeth","webdev");
+      new Student(1,"Wayne","Blockchain"),
+      new Student(2,"bruce","webdev")
     )
   );
   public List<Student> getStudents(){
